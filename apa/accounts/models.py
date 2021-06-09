0737871c6936e8089558bb13455bbaa496a8130f
inshamanowar <inshamanowar22@gmail.com>
2021-06-09 10:35:48 +0530

    preloader, bg in vision, small changes

--- apa/accounts/models.py
+++ apa/accounts/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import DefaultConnectionProxy, models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
@@ -55,7 +55,7 @@ class Account(AbstractBaseUser):
     is_active= models.BooleanField(default=False)
     is_staff= models.BooleanField(default=False)
     is_superuser= models.BooleanField(default=False)
-    profile_image=models.ImageField(upload_to=upload_location, blank=True, null=True)
+    profile_image=models.ImageField(upload_to=upload_location, blank=True, null=True, default='static/deafult.png')
     
     
     
--- apa/apa/settings.py
+++ apa/apa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')--3vd0e483xwc6iako#1gjjo99)p72ez791_u2ynqj1w7r_9@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
