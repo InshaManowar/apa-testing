fa6e6062e2655e1ff451272d56699e57454f90f1
inshamanowar <inshamanowar22@gmail.com>
2021-06-24 09:51:58 +0530

    updates

--- apa/apa/settings.py
+++ apa/apa/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'apa',
         'USER': 'root',
-        'PASSWORD':'',
+        'PASSWORD':'Ayurved17%',
         'HOST': 'localhost',
         'PORT': '3306',
     }
